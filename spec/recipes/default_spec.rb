e5ed043082f95632c646e064822789e2c402751c
Chris Aumann <me@chr4.org>
2014-03-17 17:42:06 +0100

    Add chefspec matcher for mysqld, add it to spec

--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -1,8 +1,8 @@
 require_relative '../spec_helper'
 
 describe 'mysqld::default' do
-  let(:rhel) { ChefSpec::Runner.new(platform: 'centos', version: '6.5').converge(described_recipe) }
-  let(:debian) { ChefSpec::Runner.new(platform: 'ubuntu', version: '12.04').converge(described_recipe) }
+  let(:rhel) { ChefSpec::Runner.new(platform: 'centos', version: '6.5', step_into: ['default']).converge(described_recipe) }
+  let(:debian) { ChefSpec::Runner.new(platform: 'ubuntu', version: '12.04', step_into: ['default']).converge(described_recipe) }
 
   it 'should use the correct mysql server package' do
     expect(rhel.node['mysqld']['packages']).to eq(['mysql-server'])
@@ -17,5 +17,10 @@ describe 'mysqld::default' do
   it 'shoudl use distribution specific service name' do
     expect(rhel.node['mysqld']['service_name']).to eq('mysqld')
     expect(debian.node['mysqld']['service_name']).to eq('mysql')
+  end
+
+  it 'should run mysqld_default provider' do
+    expect(rhel).to create_mysqld
+    expect(debian).to create_mysqld
   end
 end
