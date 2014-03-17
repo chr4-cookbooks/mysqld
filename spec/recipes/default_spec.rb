0da2e28eee831ed244410b328fa6f3a79ff21e6c
Chris Aumann <me@chr4.org>
2014-03-17 17:29:50 +0100

    Add chefspec tests for some important attributes

---
+++ spec/recipes/default_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../spec_helper'
+
+describe 'mysqld::default' do
+  let(:rhel) { ChefSpec::Runner.new(platform: 'centos', version: '6.5').converge(described_recipe) }
+  let(:debian) { ChefSpec::Runner.new(platform: 'ubuntu', version: '12.04').converge(described_recipe) }
+
+  it 'should use the correct mysql server package' do
+    expect(rhel.node['mysqld']['packages']).to eq(['mysql-server'])
+    expect(debian.node['mysqld']['packages']).to eq(['mysql-server'])
+  end
+
+  it 'should use distribution specific my.cnf path' do
+    expect(rhel.node['mysqld']['my.cnf_path']).to eq('/etc/my.cnf')
+    expect(debian.node['mysqld']['my.cnf_path']).to eq('/etc/mysql/my.cnf')
+  end
+
+  it 'shoudl use distribution specific service name' do
+    expect(rhel.node['mysqld']['service_name']).to eq('mysqld')
+    expect(debian.node['mysqld']['service_name']).to eq('mysql')
+  end
+end
