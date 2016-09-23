6a227495fc505b16f4d5f003f77b0b09aabbf07a
Chris Aumann <me@chr4.org>
2016-09-23 16:54:35 +0200

    Use single quotes when string interpolation is not needed

--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../spec_helper'
 
-require "chefspec/berkshelf"
+require 'chefspec/berkshelf'
 
 describe 'mysqld::default' do
   let(:debian) do
@@ -8,22 +8,22 @@ describe 'mysqld::default' do
   end
 
   let(:ubuntu_1604) do
-    ChefSpec::SoloRunner.new(platform: "ubuntu", version: "16.04").converge(described_recipe)
+    ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04').converge(described_recipe)
   end
 
   it 'should use the correct mysql server package' do
     expect(debian.node['mysqld']['mysql_packages']).to eq(%w(mysql-server))
-    expect(ubuntu_1604.node["mysqld"]["mysql_packages"]).to eq(%w(mysql-server))
+    expect(ubuntu_1604.node['mysqld']['mysql_packages']).to eq(%w(mysql-server))
   end
 
   it 'should use distribution specific my.cnf path' do
     expect(debian.node['mysqld']['my.cnf_path']).to eq('/etc/mysql/my.cnf')
-    expect(ubuntu_1604.node["mysqld"]["my.cnf_path"]).to eq("/etc/mysql/my.cnf")
+    expect(ubuntu_1604.node['mysqld']['my.cnf_path']).to eq('/etc/mysql/my.cnf')
   end
 
   it 'should use distribution specific service name' do
     expect(debian.node['mysqld']['service_name']).to eq('mysql')
-    expect(ubuntu_1604.node["mysqld"]["service_name"]).to eq("mysql")
+    expect(ubuntu_1604.node['mysqld']['service_name']).to eq('mysql')
   end
 
   it 'should run mysqld_default provider' do
@@ -31,11 +31,11 @@ describe 'mysqld::default' do
     expect(ubuntu_1604).to create_mysqld
   end
 
-  it "should use distribution and version specific attributes" do
-    expect(ubuntu_1604.node["mysqld"]["my.cnf"]["mysqld"]["key_buffer_size"]).to eq("16M")
-    expect(debian.node["mysqld"]["my.cnf"]["mysqld"]["key_buffer_size"]).to eq("16M")
+  it 'should use distribution and version specific attributes' do
+    expect(ubuntu_1604.node['mysqld']['my.cnf']['mysqld']['key_buffer_size']).to eq('16M')
+    expect(debian.node['mysqld']['my.cnf']['mysqld']['key_buffer_size']).to eq('16M')
 
-    expect(ubuntu_1604.node["mysqld"]["my.cnf"]["mysqld"]["myisam_recover_options"]).to eq("BACKUP")
-    expect(debian.node["mysqld"]["my.cnf"]["mysqld"]["myisam_recover_options"]).to eq("BACKUP")
+    expect(ubuntu_1604.node['mysqld']['my.cnf']['mysqld']['myisam_recover_options']).to eq('BACKUP')
+    expect(debian.node['mysqld']['my.cnf']['mysqld']['myisam_recover_options']).to eq('BACKUP')
   end
 end
