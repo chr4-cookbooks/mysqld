17ed6f95a3af74a8fc44e8c0f3c0bb5f39668f0f
Chris Aumann <me@chr4.org>
2016-09-23 11:08:22 +0200

    Drop rhel support, rename mariadb_apt_repository recipe

--- attributes/defaults.rb
+++ attributes/defaults.rb
@@ -26,48 +26,20 @@ default['mysqld']['mysql_packages'] = %w(mysql-server)
 default['mysqld']['mariadb_packages'] = %w(mariadb-server)
 default['mysqld']['mariadb_galera_packages'] = %w(mariadb-galera-server)
 
+# MariaDB repository configuration
 default['mysqld']['repository']['version'] = '10.1'
 default['mysqld']['repository']['mirror'] = 'http://ftp.hosteurope.de/mirror/mariadb.org/repo'
 
-# Figure out architecture and major platform version for yum repositories
-default['mysqld']['repository']['arch'] = node['kernel']['machine'] =~ /x86_64/ ? 'amd64' : 'x86'
-
-case node['platform']
-when 'redhat'
-  default['mysqld']['repository']['platform'] = "rhel#{node['platform_version'].to_i}"
-when 'centos'
-  default['mysqld']['repository']['platform'] = "centos#{node['platform_version'].to_i}"
-when 'fedora'
-  default['mysqld']['repository']['platform'] = "fedora#{node['platform_version'].to_i}"
-end
-
-case node['platform_family']
-when 'debian'
-  default['mysqld']['my.cnf_path'] = '/etc/mysql/my.cnf'
-  default['mysqld']['service_name'] = 'mysql'
-  default['mysqld']['includedir'] = true
-when 'rhel'
-  default['mysqld']['my.cnf_path'] = '/etc/my.cnf'
-  default['mysqld']['service_name'] = 'mysqld'
-  default['mysqld']['includedir'] = false
-end
+# Configure services
+default['mysqld']['my.cnf_path'] = '/etc/mysql/my.cnf'
+default['mysqld']['service_name'] = 'mysql'
+default['mysqld']['includedir'] = true
 
 # Root password, not set if nil
 default['mysqld']['root_password'] = nil
 
-# Default authentication method
-if node['platform_family'] == 'debian'
-  # Use defaults-file on Debian
-  default['mysqld']['auth'] = '--defaults-file=/etc/mysql/debian.cnf'
-else
-  if node['mysqld']['root_password']
-    # Use root with password, if password was set in the attribute
-    default['mysqld']['auth'] = "--user=root --password=#{node['mysqld']['root_password']}"
-  else
-    # Use root without a password
-    default['mysqld']['auth'] = '--user=root'
-  end
-end
+# Use defaults-file for authentication (Debian)
+default['mysqld']['auth'] = '--defaults-file=/etc/mysql/debian.cnf'
 
 # Options, only set by default/ available on MariaDB
 if node['mysqld']['use_mariadb']
@@ -93,27 +65,16 @@ default['mysqld']['my.cnf']['mysqld']['thread_stack'] = '192K'
 default['mysqld']['my.cnf']['mysqld']['thread_cache_size'] = 8
 default['mysqld']['my.cnf']['mysqld']['query_cache_limit'] = '1M'
 default['mysqld']['my.cnf']['mysqld']['query_cache_size'] = '16M'
+default['mysqld']['my.cnf']['mysqld']['pid-file'] = '/var/run/mysqld/mysqld.pid'
+default['mysqld']['my.cnf']['mysqld']['socket'] = '/var/run/mysqld/mysqld.sock'
+default['mysqld']['my.cnf']['mysqld']['basedir'] = '/usr'
+default['mysqld']['my.cnf']['mysqld']['datadir'] = '/var/lib/mysql'
+default['mysqld']['my.cnf']['mysqld']['tmpdir'] = '/tmp'
 default['mysqld']['my.cnf']['mysqld']['log_error'] = '/var/log/mysql/error.log'
 default['mysqld']['my.cnf']['mysqld']['expire_logs_days'] = 10
 default['mysqld']['my.cnf']['mysqld']['max_binlog_size'] = '100M'
 default['mysqld']['my.cnf']['mysqld']['myisam-recover-options'] = 'BACKUP'
 
-case node['platform_family']
-when 'debian'
-  default['mysqld']['my.cnf']['mysqld_safe']['socket'] = '/var/run/mysqld/mysqld.sock'
-  default['mysqld']['my.cnf']['mysqld_safe']['nice'] = 0
-  default['mysqld']['my.cnf']['mysqld_safe']['syslog'] = true
-
-  default['mysqld']['my.cnf']['mysqld']['pid-file'] = '/var/run/mysqld/mysqld.pid'
-  default['mysqld']['my.cnf']['mysqld']['socket'] = '/var/run/mysqld/mysqld.sock'
-  default['mysqld']['my.cnf']['mysqld']['basedir'] = '/usr'
-  default['mysqld']['my.cnf']['mysqld']['datadir'] = '/var/lib/mysql'
-  default['mysqld']['my.cnf']['mysqld']['tmpdir'] = '/tmp'
-
-when 'rhel'
-  default['mysqld']['my.cnf']['mysqld_safe']['log-error'] = '/var/log/mysqld.log'
-  default['mysqld']['my.cnf']['mysqld_safe']['pid-file'] = '/var/run/mysqld/mysqld.pid'
-
-  default['mysqld']['my.cnf']['mysqld']['datadir'] = '/var/lib/mysql'
-  default['mysqld']['my.cnf']['mysqld']['socket'] = '/var/lib/mysql/mysql.sock'
-end
+default['mysqld']['my.cnf']['mysqld_safe']['socket'] = '/var/run/mysqld/mysqld.sock'
+default['mysqld']['my.cnf']['mysqld_safe']['nice'] = 0
+default['mysqld']['my.cnf']['mysqld_safe']['syslog'] = true
